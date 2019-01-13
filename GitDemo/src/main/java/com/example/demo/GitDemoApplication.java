e339521bd2971e1ef33fd2451f7c38a3e50d7159
Sudhindra Vadiraj Kadekar <s_v_kadekar@yahoo.com>
2019-01-13 15:57:50 +0530

    Modified comments

--- GitDemo/src/main/java/com/example/demo/GitDemoApplication.java
+++ GitDemo/src/main/java/com/example/demo/GitDemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //Github demo change
+//Sudhindra Comments
 @SpringBootApplication
 public class GitDemoApplication {
 
